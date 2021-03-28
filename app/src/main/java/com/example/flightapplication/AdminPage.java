6dbee553e9607bab0def38855bf1bf44525475eb
bilgee23 <73483481+bilgee23@users.noreply.github.com>
2021-03-28 23:24:33 +0300

    admin girişi yapıldı

---
+++ app/src/main/java/com/example/flightapplication/AdminPage.java
@@ -0,0 +1,14 @@
+package com.example.flightapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_homepage);
+    }
+}
--- app/src/main/java/com/example/flightapplication/Login_Page.java
+++ app/src/main/java/com/example/flightapplication/Login_Page.java
@@ -17,6 +17,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Login_Page extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -24,13 +31,21 @@ public class Login_Page extends AppCompatActivity {
     private EditText loginPassword;
     private Button loginButton;
     private ProgressDialog loginProgress;
+    private DatabaseReference mAdmin;
+    private ArrayList<String> arrAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView((R.layout.activity_login_page));
 
-        main();
+        loginEmail = findViewById(R.id.inputEmail4);
+        loginPassword = findViewById(R.id.inputPassword3);
+        loginButton = findViewById(R.id.buttonLogin);
+        mAuth = FirebaseAuth.getInstance();
+        mAdmin = FirebaseDatabase.getInstance().getReference("admins");
+        loginProgress = new ProgressDialog(this);
+        arrAdmin = new ArrayList<>();
 
         TextView btn = findViewById(R.id.textViewtoRegister);
         btn.setOnClickListener(v -> startActivity(new Intent(Login_Page.this, Register_Page.class)));
@@ -44,7 +59,7 @@ public class Login_Page extends AppCompatActivity {
                 String email = loginEmail.getText().toString();
                 String password = loginPassword.getText().toString();
 
-                 if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+                if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
                     loginProgress.setTitle("Signing in");
                     loginProgress.setMessage("Please wait, Signing in");
                     loginProgress.setCanceledOnTouchOutside(false);
@@ -52,8 +67,7 @@ public class Login_Page extends AppCompatActivity {
                     login_user(email, password);
 
 
-                }
-                else {
+                } else {
                     Toast.makeText(Login_Page.this, "Please fill in the blank spaces", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -63,29 +77,43 @@ public class Login_Page extends AppCompatActivity {
     }
 
     private void login_user(String email, String password) {
-
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        mAdmin.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    loginProgress.dismiss();
-                    Toast.makeText(Login_Page.this, "Login successful", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(Login_Page.this, UserHomePage.class);
-                    startActivity(intent);
-                }else{
-                    loginProgress.dismiss();
-                    Toast.makeText(Login_Page.this, "Login failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                        arrAdmin.add((String) snapshot1.getValue());
+                    }
+                    for (int i = 0; i < arrAdmin.size(); i++) {
+                        if (email.equals(arrAdmin.get(i))) {
+                            Intent intent = new Intent(Login_Page.this, AdminPage.class);
+                            startActivity(intent);
+                        } else {
+                            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        loginProgress.dismiss();
+                                        Toast.makeText(Login_Page.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(Login_Page.this, UserHomePage.class);
+                                        startActivity(intent);
+                                    } else {
+                                        loginProgress.dismiss();
+                                        Toast.makeText(Login_Page.this, "Login failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
 
+                                    }
+                                }
+                            });
+                        }
+                    }
                 }
             }
-        });
-    }
 
-    void main( ) {
-        loginEmail = findViewById(R.id.inputEmail4);
-        loginPassword = findViewById(R.id.inputPassword3);
-        loginButton = findViewById(R.id.buttonLogin);
-        mAuth = FirebaseAuth.getInstance();
-        loginProgress = new ProgressDialog(this);
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
